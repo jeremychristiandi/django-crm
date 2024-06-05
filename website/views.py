4160054e10a76955e72080cad7ea0bebbfc5ae63
Jeremy <jeremychristiandi89@gmail.com>
2024-06-05 12:42:37 +0700

    add registration

--- website/views.py
+++ website/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+def home(request):
+    # Check to see if user is logged in
+    status = None
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authenticate the user
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Login successful.")
+            status = 'success'
+        else:
+            messages.error(request,"An error has been occurred while logging in.")
+            status = 'error'
+    
+    return render(request, "home.html", {
+        'status': status
+    })
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "Successfully logged out.")
+
+    return redirect('home')
+
+def register_user(request):
+    return render(request, "register.html")
